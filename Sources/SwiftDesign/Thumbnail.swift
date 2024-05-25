24f70c3143eb00567efea5808f3ca3e076e07ad0
Hieu Tran <tranduchieu@me.com>
2024-05-25 23:03:31 +0700

    add Thumbnail

---
+++ Sources/SwiftDesign/Thumbnail.swift
@@ -0,0 +1,65 @@
+//
+//  Thumbnail.swift
+//
+//
+//  Created by Hieu Tran on 25/05/2024.
+//
+
+import SwiftUI
+
+public struct AsyncThumbnail: View {
+    let imageUrl: URL?
+    let aspectRatio: CGFloat
+    let cornerRadius: CGFloat
+    
+    public init(
+        imageUrl: URL?,
+        aspectRatio: CGFloat = 16 / 10,
+        cornerRadius: CGFloat = 8
+    ) {
+        self.imageUrl = imageUrl
+        self.aspectRatio = aspectRatio
+        self.cornerRadius = cornerRadius
+    }
+    
+    public var body: some View {
+        if let imageUrl = imageUrl {
+            AsyncImage(url: imageUrl) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity
+                    )
+                    .aspectRatio(aspectRatio, contentMode: .fit)
+                    .clipped()
+                    .cornerRadius(cornerRadius)
+                
+            } placeholder: {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fill(
+                        Color(UIColor.secondarySystemBackground)
+                    )
+                    .aspectRatio(aspectRatio, contentMode: .fit)
+                
+            }
+        } else {
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .fill(
+                    Color(UIColor.secondarySystemBackground)
+                )
+                .aspectRatio(aspectRatio, contentMode: .fit)
+        }
+    }
+}
+
+#Preview {
+    AsyncThumbnail(
+        imageUrl: URL(string: "https://picsum.photos/200/300"),
+        aspectRatio: 16 / 9,
+        cornerRadius: 8
+    )
+}

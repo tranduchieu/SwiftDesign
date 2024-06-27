8106a209c953c65aa517cfa5ab9dd799c80c150d
Hieu Tran <tranduchieu@me.com>
2024-06-27 13:06:26 +0700

    update playlistcard

--- Sources/SwiftDesign/Video/PlaylistCard3.swift
+++ Sources/SwiftDesign/Video/PlaylistCard3.swift
@@ -8,97 +8,126 @@
 import SwiftUI
 
 public struct PlaylistCard3: View {
-    let videoItem: VideoItem
+    let playlistItem: PlaylistItem
     let cardSize: CardSize
     
+    var thumbnail: AsyncThumbnail {
+        if var thumbnail = playlistItem.thumbnail {
+            thumbnail.cornerRadius = 0
+            thumbnail.aspectRatio = 16 / 9
+            return thumbnail
+        } else {
+            return AsyncThumbnail(imageUrl: nil, aspectRatio: 16 / 9, cornerRadius: 0)
+        }
+    }
+
+    
     public init(
-        videoItem: VideoItem,
+        playlistItem: PlaylistItem,
         cardSize: CardSize = .small
     ) {
-        self.videoItem = videoItem
+        self.playlistItem = playlistItem
         self.cardSize = cardSize
     }
     
+    let cornerRadius: CGFloat = 12
+    
+    
     public var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            if let thumbnail = videoItem.thumbnail {
-                AsyncThumbnailView(asyncThumbnail: thumbnail)
-                    .overlay {
-                            HStack {
-                                
-                                Spacer()
-                                if let durationFormatted = videoItem.durationFormatted {
-                                    HStack(spacing: 4) {
-                                        Image(systemName: "play.fill")
-                                        Text(videoItem.durationLeftFormatted != nil ? "\(videoItem.durationLeftFormatted!) left" : durationFormatted)
-                                    }
-                                        .font(cardSize == .large ? .subheadline : .caption)
-                                        .fontWeight(.semibold)
-                                        .fontDesign(.rounded)
-                                        .lineLimit(1)
-                                        .padding(4)
-                                        .background(Color.black.opacity(0.6))
-                                        .foregroundStyle(.white)
-                                        .cornerRadius(cardSize == .large ? 6 : 4)
-//                                        .offset(x: 4, y: 4)
-//                                        .padding(4)
-                                        .overlay {
-                                            if let duration = videoItem.duration, let lastPlaybackPosition = videoItem.lastPlaybackPosition {
-                                                VStack {
-                                                    Spacer()
-                                                    PlaybackProgressView(lastPlaybackPosition: lastPlaybackPosition, duration: duration)
-                                                        .padding(1)
-                                                }
-                                            }
-                                        }
-                                }
-                            }
-                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-                            
 
-                        
-                        .padding(.horizontal, cardSize == .large ? 12 : cardSize == .medium ? 8 : 4)
-                        .padding(.vertical, cardSize == .large ? 8 : 4)
-                    }
-                    .overlay {
-                        RoundedRectangle(cornerRadius: thumbnail.cornerRadius)
-                            .stroke(.thinMaterial, lineWidth: 1) // Add the inner border
-                    }
-            }
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text(videoItem.title)
-                    .font(cardSize == .large ? .body : .subheadline)
-                    .lineLimit(2)
-                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                if let subtitle = videoItem.subtitle{
-                    Text(subtitle)
-                        .font(.caption)
-                        .lineLimit(1)
-                        .foregroundStyle(.secondary)
+        
+        RoundedRectangle(cornerRadius: cornerRadius)
+            .fill(.clear)
+            .aspectRatio( 16 / 15, contentMode: .fit)
+            .overlay {
+                VStack {
+                    Spacer()
+                    
+                    RoundedCornersShape(corners: [.bottomLeft, .bottomRight], radius: cornerRadius).fill(.ultraThinMaterial)
+                        .background {
+                            VStack {
+                                Spacer()
+                                Thumbnail()
+                                    .clipShape(RoundedCornersShape(corners: [.bottomLeft, .bottomRight], radius: 18))
+                            }
+                            
+                        }
+                        .overlay {
+                            VStack(alignment: .leading, spacing: 4) {
+                                Rectangle()
+                                    .fill(.clear)
+                                    .aspectRatio(thumbnail.aspectRatio, contentMode: .fit)
+                                
+                                Text(playlistItem.title)
+                                    .font(cardSize == .large ? .body : cardSize == .medium ? .subheadline : .caption)
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                                    Text("\(playlistItem.videoCount) videos")
+                                        .font(.caption)
+                                        .lineLimit(1)
+                                        .foregroundStyle(.secondary)
+                                
+                            }
+                            .padding(.horizontal, cardSize == .large ? 12 : 8)
+                            .padding(.vertical, 8)
+                        }
                 }
             }
-            .padding(.horizontal, cardSize == .large ? 12 : 8)
-        }
+            .overlay {
+                VStack{
+                    Thumbnail()
+                        .clipShape(RoundedCornersShape(corners: [.topLeft, .topRight], radius: cornerRadius))
+                        .overlay {
+                                HStack {
+                                    
+                                    Spacer()
+                
+                                        HStack(spacing: 4) {
+                                            Image(systemName: "play.square.stack.fill")
+                                            Text("PLAYLIST")
+                                        }
+                                            .font(cardSize == .large ? .subheadline : .caption)
+                                            .fontWeight(.semibold)
+                                            .fontDesign(.rounded)
+                                            .lineLimit(1)
+                                            .padding(4)
+                                            .background(Color.black.opacity(0.6))
+                                            .foregroundStyle(.white)
+                                            .cornerRadius(cardSize == .large ? 6 : 4)
+
+                                    
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                                
+
+                            
+                            .padding(.horizontal, cardSize == .large ? 12 : cardSize == .medium ? 8 : 4)
+                            .padding(.vertical, cardSize == .large ? 8 : 4)
+                        }
+                    
+                    Spacer()
+                }
+            }
+
+            .overlay(
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(.thinMaterial, lineWidth: 1) // Add the inner border
+            )
+    }
+    
+    @ViewBuilder
+    func Thumbnail() -> some View {
+        AsyncThumbnailView(asyncThumbnail: thumbnail)
     }
 }
 
 #Preview {
-    PlaylistCard3(
-        videoItem: VideoItem(
-            id: "uSuIl1Fsz3g",
-            title: "WWDC23: Explore enhancements to App Intents",
-            author: "Subtitle",
-            thumbnail: AsyncThumbnail(
-                imageUrl: URL(string: "https://i.ytimg.com/vi/uSuIl1Fsz3g/maxresdefault.jpg"),
-                aspectRatio: 16 / 9,
-                cornerRadius: 8
-            ),
-            youtubeUrl: URL(string: "https://youtube.com/watch?v=uSuIl1Fsz3g"),
-            duration: 3600,
-            lastPlaybackPosition: 1800
-        ),
-        cardSize: .large
-    )
+    LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 200))]) {
+        PlaylistCard3(
+            playlistItem: PlaylistItem.examples[0],
+            cardSize: .small
+        )
+    }
+    .padding()
     .preferredColorScheme(.dark)
 }
--- Sources/SwiftDesign/Video/VideoCard2.swift
+++ Sources/SwiftDesign/Video/VideoCard2.swift
@@ -14,9 +14,10 @@ public struct VideoCard2: View {
     var thumbnail: AsyncThumbnail {
         if var thumbnail = videoItem.thumbnail {
             thumbnail.cornerRadius = 0
+            thumbnail.aspectRatio = 16 / 9
             return thumbnail
         } else {
-            return AsyncThumbnail(imageUrl: nil, aspectRatio: 16 / 9)
+            return AsyncThumbnail(imageUrl: nil, aspectRatio: 16 / 9, cornerRadius: 0)
         }
     }
 
@@ -29,60 +30,128 @@ public struct VideoCard2: View {
         self.cardSize = cardSize
     }
     
+    let cornerRadius: CGFloat = 12
+    
     
     public var body: some View {
 
         
-        RoundedRectangle(cornerRadius: 18)
+        RoundedRectangle(cornerRadius: cornerRadius)
             .fill(.clear)
-            .aspectRatio(1, contentMode: .fit)
+            .aspectRatio( 16 / 15, contentMode: .fit)
             .overlay {
                 VStack {
                     Spacer()
                     
-                    RoundedCornersShape(corners: [.bottomLeft, .bottomRight], radius: 18).fill(.thinMaterial)
-                        .background(
-                            AsyncThumbnailView(asyncThumbnail: thumbnail)
-                                .clipShape(RoundedCornersShape(corners: [.bottomLeft, .bottomRight], radius: 18))
-                            
-                            
-                        )
-                        
+                    RoundedCornersShape(corners: [.bottomLeft, .bottomRight], radius: cornerRadius).fill(.ultraThinMaterial)
+                        .background {
+                            VStack {
+                                Spacer()
+                                Thumbnail()
+                                    .clipShape(RoundedCornersShape(corners: [.bottomLeft, .bottomRight], radius: 18))
+                                
+                            }
+                        }
+                        .overlay {
+                            VStack(alignment: .leading, spacing: 4) {
+                                Rectangle()
+                                    .fill(.clear)
+                                    .aspectRatio(thumbnail.aspectRatio, contentMode: .fit)
+                                
+                                Text(videoItem.title)
+                                    .font(cardSize == .large ? .body : cardSize == .medium ? .subheadline : .caption)
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                                if let subtitle = videoItem.subtitle{
+                                    Text(subtitle)
+                                        .font(.caption)
+                                        .lineLimit(1)
+                                        .foregroundStyle(.secondary)
+                                }
+                            }
+                            .padding(.horizontal, cardSize == .large ? 12 : 8)
+                            .padding(.vertical, 8)
+                        }
                 }
             }
             .overlay {
                 VStack{
-                    AsyncThumbnailView(asyncThumbnail:  thumbnail)
-                        .clipShape(RoundedCornersShape(corners: [.topLeft, .topRight], radius: 18))
-                    
+                    Thumbnail()
+                        .clipShape(RoundedCornersShape(corners: [.topLeft, .topRight], radius: cornerRadius))
+                        .overlay {
+                                HStack {
+                                    
+                                    Spacer()
+                                    if let durationFormatted = videoItem.durationFormatted {
+                                        HStack(spacing: 4) {
+                                            Image(systemName: "play.fill")
+                                            Text(videoItem.durationLeftFormatted != nil ? "\(videoItem.durationLeftFormatted!) left" : durationFormatted)
+                                        }
+                                            .font(cardSize == .large ? .subheadline : .caption)
+                                            .fontWeight(.semibold)
+                                            .fontDesign(.rounded)
+                                            .lineLimit(1)
+                                            .padding(4)
+                                            .background(Color.black.opacity(0.6))
+                                            .foregroundStyle(.white)
+                                            .cornerRadius(cardSize == .large ? 6 : 4)
+    //                                        .offset(x: 4, y: 4)
+    //                                        .padding(4)
+                                            .overlay {
+                                                if let duration = videoItem.duration, let lastPlaybackPosition = videoItem.lastPlaybackPosition {
+                                                    VStack {
+                                                        Spacer()
+                                                        PlaybackProgressView(lastPlaybackPosition: lastPlaybackPosition, duration: duration)
+                                                            .padding(1)
+                                                    }
+                                                }
+                                            }
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                                
+
+                            
+                            .padding(.horizontal, cardSize == .large ? 12 : cardSize == .medium ? 8 : 4)
+                            .padding(.vertical, cardSize == .large ? 8 : 4)
+                        }
                     
                     Spacer()
                 }
             }
 
             .overlay(
-                RoundedRectangle(cornerRadius: 18)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .stroke(.thinMaterial, lineWidth: 1) // Add the inner border
             )
+    }
+    
+    @ViewBuilder
+    func Thumbnail() -> some View {
+        AsyncThumbnailView(asyncThumbnail:  thumbnail)
     }
 }
 
 #Preview {
-    VideoCard2(
-        videoItem: VideoItem(
-            id: "uSuIl1Fsz3g",
-            title: "WWDC23: Explore enhancements to App Intents",
-            author: "Subtitle",
-            thumbnail: AsyncThumbnail(
-                imageUrl: URL(string: "https://i.ytimg.com/vi/uSuIl1Fsz3g/maxresdefault.jpg"),
-                aspectRatio: 16 / 9,
-                cornerRadius: 8
+    LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 200))]) {
+        VideoCard2(
+            videoItem: VideoItem(
+                id: "uSuIl1Fsz3g",
+                title: "WWDC23: Explore enhancements to App Intents",
+                subtitle: "Subtitle",
+                author: "Subtitle",
+                thumbnail: AsyncThumbnail(
+                    imageUrl: URL(string: "https://i.ytimg.com/vi/uSuIl1Fsz3g/maxresdefault.jpg"),
+                    aspectRatio: 16 / 9,
+                    cornerRadius: 8
+                ),
+                youtubeUrl: URL(string: "https://youtube.com/watch?v=uSuIl1Fsz3g"),
+                duration: 3600,
+                lastPlaybackPosition: 1800
             ),
-            youtubeUrl: URL(string: "https://youtube.com/watch?v=uSuIl1Fsz3g"),
-            duration: 3600,
-            lastPlaybackPosition: 1800
-        ),
-        cardSize: .large
-    )
+            cardSize: .small
+        )
+    }
+    .padding()
     .preferredColorScheme(.dark)
 }
